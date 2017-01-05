b2bd5a7c461a608974466b11ed4679833d1cdc26
Billy Purvis <purvisbilly@outlook.com>
2017-01-05 21:01:15 +0000

    uses lifeCycle hook to call vue instance method

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+## [0.0.1] - [Unreleased] - [Billy Purvis] - 05-01-2017 
+
+### Added 
+- .babelrc missing file
+- .editorconfig missing file
+- .eslintignore missing file
+- .eslintrc.js missing file
+
+### Changed
+- added 'created' lifecycle hook to request methods automatically
+
+### Removed
+- Slick content
--- src/components/main-banner.vue
+++ src/components/main-banner.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+/* eslint-env  jquery */
 export default {
   data () {
     return {
@@ -20,22 +21,12 @@ export default {
       ]
     }
   },
-
+  created: function () {
+    this.slick() // uses lifeCycle hook to call method once the Vue instance is made
+  },
   methods: {
-    initSlick: function () {
-      this.$nextTick(function () {
-        $('.banner-carousel').slick({
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 5000,
-          arrows: true,
-          prevArrow: '<svg version="1.1" class="left-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 487.622 487.622" style="enable-background:new 0 0 487.622 487.622;" xml:space="preserve"><g><g><circle style="fill:#D21819;" cx="243.811" cy="243.811" r="243.811"/><path style="fill:#C20809;" d="M486.922,262.38L365.708,121.912l-131.415,87.582l-112.381,34.317l202.325,230.212 C413.786,442.74,479.517,360.705,486.922,262.38z"/><polygon style="fill:#FFFFFF;" points="121.913,243.811 365.708,121.912 281.928,243.811 365.708,365.708 "/></g></g></svg>',
-          nextArrow: '<svg version="1.1" class="right-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 487.622 487.622" style="enable-background:new 0 0 487.622 487.622;" xml:space="preserve"><g><g><circle style="fill:#D21819;" cx="243.811" cy="243.811" r="243.811"/><path style="fill:#C20809;" d="M486.922,262.38L365.708,121.912l-131.415,87.582l-112.381,34.317l202.325,230.212 C413.786,442.74,479.517,360.705,486.922,262.38z"/><polygon style="fill:#FFFFFF;" points="121.913,243.811 365.708,121.912 281.928,243.811 365.708,365.708 "/></g></g></svg>',
-          speed: 1500
-        })
-      })
+    slick: function () {
+      console.log('Slick')
     }
   }
 }
